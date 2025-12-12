2146425208cdd4791c50badf2ddeff59564290bc
CarloMart88 <carlo.martino1988@gmail.com>
2025-12-12 09:59:02 +0100

    feat second snack

--- snack.test.js
+++ snack.test.js
@@ -10,3 +10,10 @@ test("La funzione getInitials restituisce le iniziali di un nome completo. ",()=
   expect(getInitials("Carlo")).toBe("C");
 
 })
+/*
+🏆 Snack 2
+Creare un test che verifichi la seguente descrizione:
+
+👉 "La funzione createSlug restituisce una stringa in lowercase."*/
+
+test("La funzione createSlug restituisce una stringa in lowercase.",()=>{})
